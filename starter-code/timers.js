b665d0c5a4dd2c683a6a3865727fb7525e0ca5d5
leeadrian841 <leeadrian841@gmail.com>
2016-09-28 20:47:20 +0800

    updated

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,34 @@
-// your code here:
+var body = document.querySelector('body')
+var reset = body.querySelector('#reset')
+var start = body.querySelector('#start')
+var pause = body.querySelector('#pause')
+var seconds = 0
+var timerId
+
+// var updateButton = setInterval(updateTime, 1000)
+// var pauseButton =
+// var resetButton = clearInterval()
+
+function updateTime () {
+  var head1 = body.querySelector('#timer')
+  head1.textContent = 'Time elapsed: ' + seconds + 's'
+  seconds++
+}
+function updateClick () {
+  timerId = setInterval(updateTime, 1000)
+}
+
+function pauseClick () {
+  clearInterval(timerId)
+}
+
+function resetClick () {
+  clearInterval(timerId)
+  var head1 = body.querySelector('#timer')
+  head1.textContent = 'Stop Watch'
+  seconds = 0
+}
+
+start.addEventListener('click', updateClick)
+pause.addEventListener('click', pauseClick)
+reset.addEventListener('click', resetClick)
